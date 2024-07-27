1dbd90f37269e874cc1cb7d66de7846ad07085cc
EshayDev <eshay.dev@gmail.com>
2024-07-27 17:16:21 +1000

    combined into one script

--- cloudflare_tunnel_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Set up CloudFlare Tunnel
-
-echo "Setting up CloudFlare Tunnel..."
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
-dpkg -i cloudflared.deb
-echo "Copy the "If you already have cloudflared installed on your machine" command and paste it below"
-echo "Command Provided by CloudFlare:"
